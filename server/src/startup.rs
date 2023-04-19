49980cc14c99812cfe75630d1d4780cc45293a94
NinjaLikesCheez <NinjaLikesCheez@users.noreply.github.com>
2023-04-19 18:26:17 +0200

    WIP: try get the connection to be shared

--- server/src/startup.rs
+++ server/src/startup.rs
@@ -1,12 +1,19 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpServer};
 use actix_web::dev::Server;
+use diesel::{SqliteConnection, r2d2};
 use std::net::TcpListener;
 
 use crate::routes;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-	let server = HttpServer::new(|| {
+	let manager = r2d2::ConnectionManager::<SqliteConnection>::new("database/pixelbank.db");
+	let pool = r2d2::Pool::builder()
+		.build(manager)
+		.expect("Failed to load SQLite database");
+
+	let server = HttpServer::new(move || {
 			App::new()
+			.app_data(web::Data::new(pool.clone()))
 			.route("/ping", web::get().to(routes::ping))
 			.route("/user", web::post().to(routes::create_user))
 		})
